87520062d76113304620d699ab6b690e2e506f66
JkTo7 <jtodd1158@gmail.com>
2021-01-07 16:21:15 -0600

    Update Demons.py

--- Demons.py
+++ Demons.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Aug  1 11:11:14 2019
-
-@author: JO20048019
-"""
 
 print("Face your demons. \nFace your fears. \nThe more you run, the more will come. \nTurn around make them run from you.")
